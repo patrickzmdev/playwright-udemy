73ce0fa77cafb0b5bf89adc1ad29b16ddb78fdad
Patrick <patrick.medeiros@triersistemas.com.br>
2025-06-05 17:48:14 -0300

    update

--- tests/support/actions/Tvshows.js
+++ tests/support/actions/Tvshows.js
@@ -13,4 +13,15 @@ export class Tvshows {
     async submit() {
         await this.page.getByRole('button', { name: 'Cadastrar' }).click();
     }
+
+    async createTvShow(tvshow) {
+        await this.goForm();
+        await this.page.getByLabel("Titulo da série").fill(tvshow.title);
+        await this.page.getByLabel("Sinopse").fill(tvshow.overview);
+        await this.page.locator('#select_company_id .react-select__indicator').click();
+        await this.page.locator('.react-select__option').filter({hasText: tvshow.company}).click();
+        await this.page.locator('#select_year .react-select__indicator').click();
+        await this.page.locator('.react-select__option').filter({hasText: tvshow.release_year}).click();
+        await this.page.locator("Temporadas")
+    }
 };
